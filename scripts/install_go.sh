e43131cbf64744d4ef14ac146501907aba16e42c
Samuel Juarez <s.juarez.p@icloud.com>
2021-12-12 13:11:24 -0500

    installing tesseract

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -18,7 +18,7 @@ function main() {
 
 
 	# Install Leptonica from Source
-	wget --no-cookies --no-check-certificate  http://www.leptonica.com/source/leptonica-1.78.0.tar.gz
+	wget --no-cookies --no-check-certificate  http://www.leptonica.org/source/leptonica-1.78.0.tar.gz
 	
 	tar -zxvf leptonica-1.78.0.tar.gz
 	cd leptonica-1.78.0
