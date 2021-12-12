45db4691302bb38b23a3e2f8294063bcae3e3be3
Samuel Juarez <s.juarez.p@icloud.com>
2021-12-12 12:52:22 -0500

    installing tesseract

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -6,15 +6,15 @@ set -o pipefail
 
 function main() {
 	echo "-----> Installing Tesseract - SJP:i"
-	apt-get update -y
-	apt-get install -y yum
+	#apt-get update -y
+	#apt-get install -y yum
 	# (Estimated Time of Completion: 45 minutes)
 	# Instructions taken (and slightly modified) from https://github.com/EisenVault/install-tesseract-redhat-centos/blob/master/install-tesseract.sh
 	cd /opt
 	# The following line will take 30 minutes to install.
-	yum -y update
-	yum -y install libstdc++ autoconf automake libtool autoconf-archive pkg-config gcc gcc-c++ make libjpeg-devel libpng-devel libtiff-devel zlib-devel
-	yum group install -y "Development Tools"
+	#yum -y update
+	#yum -y install libstdc++ autoconf automake libtool autoconf-archive pkg-config gcc gcc-c++ make libjpeg-devel libpng-devel libtiff-devel zlib-devel
+	#yum group install -y "Development Tools"
 
 
 	# Install Leptonica from Source
