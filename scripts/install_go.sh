802c1f5723cc6ad4dd1eeb1ecddc9a9b025fd429
Samuel Juarez <s.juarez.p@icloud.com>
2021-12-12 13:04:55 -0500

    installing tesseract

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -16,9 +16,7 @@ function main() {
 	#yum -y install libstdc++ autoconf automake libtool autoconf-archive pkg-config gcc gcc-c++ make libjpeg-devel libpng-devel libtiff-devel zlib-devel
 	#yum group install -y "Development Tools"
 
-  mkdir sjp_install
-	chmod 777 sjp_install
-	cd sjp_install
+
 	# Install Leptonica from Source
 	wget http://www.leptonica.com/source/leptonica-1.78.0.tar.gz
 	
